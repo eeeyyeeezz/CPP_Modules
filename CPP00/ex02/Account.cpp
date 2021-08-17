268c5d6f5b91e464c94bca4485ce412477d6d967
eeeyyeeezz <eximmlich@gmail.com>
2021-08-17 17:41:55 +0300

    ex02

--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -3,15 +3,54 @@
 #include <ctime>
 #include "Account.hpp"
 
+Account::Account(){
+	this->_nbAccounts = 0;
+	this->_accountIndex = -1;
+	this->_amount = 0;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+}
 
-Account::Account(int initial_deposit)
-{
-	
-	
+Account::Account(int initial_deposit){
+	this->_accountIndex++;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+	this->_amount = initial_deposit;
+	_displayTimestamp();
+	this->_nbAccounts = this->_accountIndex;
 };
 
-void	Account::_displayTimestamp(void)
-{
+int		Account::getNbAccounts(){
+return _nbAccounts;
+}
+
+int		Account::getTotalAmount(){
+return _totalAmount;
+}
+
+int		Account::getNbDeposits(){
+return _totalNbDeposits;
+}
+
+int		Account::getNbWithdrawals(){
+return _totalNbWithdrawals;
+}
+
+void	Account::displayAccountsInfos(){
+
+}
+
+
+void	Account::displayStatus() const {
+	this->displayStatus();
+	std::cout << " index:" << this->_accountIndex;
+	std::cout << ";amount:" << this->_amount;
+	std::cout << ";deposits:" << this->_nbDeposits;
+	std::cout << ";withdrawals:" << this->_nbWithdrawals;
+	std::cout << std::endl;
+}
+
+void	Account::_displayTimestamp(){
 	auto start = std::chrono::system_clock::now();
 	auto end = std::chrono::system_clock::now();
 
@@ -19,11 +58,12 @@ void	Account::_displayTimestamp(void)
 	std::time_t end_time = std::chrono::system_clock::to_time_t(end);
 
 	std::cout << "TIME: [" << std::ctime(&end_time)
-				<< "] TIME???: " << elapsed_seconds.count() << "s\n";
+				<< "] TIME???: " << elapsed_seconds.count() << std::endl;
 }
 
-
-// Account::~Account
-// {
-	
-// };
+Account::~Account(){
+	this->displayStatus();
+	std::cout << " index:" << this->_accountIndex;
+	std::cout << ";amount:" << this->_amount;
+	std::cout << ";closed" << std::endl;
+};
