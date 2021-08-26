6f13878be401e5e7d1b346e4bf22ff2054c63a03
eeeyyeeezz <eximmlich@gmail.com>
2021-08-26 14:09:21 +0300

    upd

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
-using namespace std;
 
 int		main(int argc, char **argv)
 {
 	if (argc == 1)
-		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else 
 	{
 		for (int i = 1; argv[i]; i++)
 		{
 			for (int j = 0; argv[i][j]; j++)
 				argv[i][j] = toupper(argv[i][j]);
-			cout << argv[i];
+			std::cout << argv[i];
 		}
 	}
-	cout << endl;
+	std::cout << std::endl;
 	return (0);
 }
