dbfd88de71270372507f86b98cc930c5b0376b72
eeeyyeeezz <eximmlich@gmail.com>
2021-09-03 14:01:57 +0300

    ex03 wtf

---
+++ CPP04/ex03/AMateria.hpp
@@ -0,0 +1,26 @@
+#ifndef AMATERIA
+#define AMATERIA
+#include <iostream>
+
+class AMateria{
+	protected:
+
+	public:
+	AMateria (std::string const &type);
+	
+	std::string const &getType() const;
+	virtual AMateria *clone() const = 0;
+	virtual void use(ICharacter &target);
+};
+
+class ICharacter{
+	public:
+		// virtual ~ICharacter () { }
+		virtual std::string const &getName() const = 0;
+		virtual void equip(AMateria *m) = 0;
+		virtual void unequip(int idx) = 0;
+		virtual void use(int idx, ICharacter &target) = 0;
+};
+
+#endif
+ 
