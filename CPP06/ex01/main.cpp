1f59fe32032e2fd8dc014425bb13e942ef703bc1
eeeyyeeezz <eximmlich@gmail.com>
2021-09-13 18:04:12 +0300

    day06 done :)))

--- CPP06/ex01/main.cpp
+++ CPP06/ex01/main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+struct Data { std::string _array; };
+
+uintptr_t serialize(Data *ptr) { return(reinterpret_cast<uintptr_t>(ptr)); };
+
+Data *deserialize(uintptr_t raw) { return (reinterpret_cast<Data *>(raw)); }
+
 int			main(){
+	Data		*ptr = new Data;
+	ptr->_array = "Yes";
 	
+	uintptr_t	tmp = serialize(ptr);
+	Data		*newPtr = deserialize(tmp);
+
+	std::cout <<  newPtr->_array << std::endl;
+	delete ptr;
 }
---
+++ CPP06/ex02/main.cpp
@@ -0,0 +1,53 @@
+#include "../colors.hpp"
+#include <iostream>
+
+class Base { public: virtual ~Base() { std::cout << RED << "Base destructor has been created ( ͡° ͜ʖ ͡°)\n"; } };
+
+class A : public Base { };
+class B : public Base { };
+class C : public Base { };
+
+Base *generate(){
+	srand(time(0));
+	int rand = std::rand();
+
+	if (!(rand % 3)){ return (new A); } 
+	else if (rand % 3 == 1) { return (new B); }
+	else if (rand % 3 == 2) { return (new C); };
+	return (NULL);
+}
+ 
+void	identify(Base *ptr){
+	if (dynamic_cast<A *>(ptr))
+		std::cout << BRIGHT_GREEN << "[A] ( ͡° ل͜ ͡°)\n";
+	else if (dynamic_cast<B *>(ptr))
+		std::cout << BRIGHT_GREEN << "[B] ( ͡° ل͜ ͡°)\n";
+	else if (dynamic_cast<C *>(ptr))
+		std::cout << BRIGHT_GREEN << "[C] ( ͡° ل͜ ͡°)\n";
+	else
+		std::cout << BLACK_RED << "TOTAL ERRROOOOOR ◥(ฅº￦ºฅ)◤\n";
+}
+
+void	identify(Base &ptr){
+	if (dynamic_cast<A *>(&ptr))
+		std::cout << BRIGHT_GREEN << "[A] ( ͡° ل͜ ͡°)\n";
+	else if (dynamic_cast<B *>(&ptr))
+		std::cout << BRIGHT_GREEN << "[B] ( ͡° ل͜ ͡°)\n";
+	else if (dynamic_cast<C *>(&ptr))
+		std::cout << BRIGHT_GREEN << "[C] ( ͡° ل͜ ͡°)\n";
+	else
+		std::cout << BLACK_RED << "TOTAL ERRROOOOOR ◥(ฅº￦ºฅ)◤\n";
+}
+
+int		main(){	
+	Base	*ptr = generate();
+	identify(ptr);
+
+	Base	*ptr2 = generate();
+	identify(*ptr2);
+
+	Base	*wrongPtr;
+	identify(wrongPtr);
+	delete ptr;
+	delete ptr2;
+}
