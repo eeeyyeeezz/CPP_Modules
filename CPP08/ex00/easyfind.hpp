7c4f271ac5ab87266446addb2c1ec57f93f5ef7c
eeeyyeeezz <eximmlich@gmail.com>
2021-09-24 16:30:56 +0300

    ex00

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -1,8 +1,21 @@
 #ifndef EASYFIND
 #define EASYFIND
+#include "../colors.hpp"
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <deque>
+#include <string>
 
+template <typename T>
+typename T::iterator easyfind(T &ctr, int num){
+	typename T::iterator	i;
 
-
-
+	i = std::find(ctr.begin(), ctr.end(), num);
+	if (i == ctr.end())
+		throw std::string("Number not found");
+	return (i);
+}
 
 #endif
--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -1,5 +1,18 @@
 #include "easyfind.hpp"
 
 int			main(){
+	std::vector<int> intVector;
+	std::list<int>	intList;
+
+	try{
+		for (int i = 0; i < 10; i++)
+			intVector.push_back(i);
+		for (int i = 0; i < static_cast<int>(intVector.size()); i++)
+			std::cout << WHITE << "INT VECTOR["<< i << "]: " << BLUE << intVector[i] << NORMAL << std::endl;
+		std::cout << std::endl; 
 	
+		std::cout << WHITE << "LOOKING FOR 4: [" << *easyfind(intVector, 4) << "]" << NORMAL << std::endl;
+		std::cout << WHITE << "LOOKING FOR 9: [" << *easyfind(intVector, 9) << "]" << NORMAL << std::endl;
+		std::cout << WHITE << "LOOKING FOR 42: " << *easyfind(intVector, 42) << "]" << NORMAL << std::endl;
+	} catch (std::string exc) { std::cout << BLACK_RED << "ERROR IS: " << RED << exc << NORMAL << std::endl; }
 }
